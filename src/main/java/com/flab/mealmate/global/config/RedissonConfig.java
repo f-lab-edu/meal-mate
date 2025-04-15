df1d02440c97f9c3b32056164280adf2529ba7fd
sia <aaay8228@gmail.com>
2025-04-15 11:39:18 +0900

    feat(redis): apply distributed lock and implement dynamic key resolver

---
+++ src/main/java/com/flab/mealmate/global/config/RedissonConfig.java
@@ -0,0 +1,29 @@
+package com.flab.mealmate.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!test")
+public class RedissonConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedissonClient redissonClient() {
+		RedissonClient redisson = null;
+		Config config = new Config();
+		config.useSingleServer().setAddress(redisHost + ":" + redisPort);
+		redisson = Redisson.create(config);
+		return redisson;
+	}
+}
