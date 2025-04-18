af2544ce34770cb9ac38ba45c89be605fae4821e
sia <93904773+yooseung20@users.noreply.github.com>
2025-04-18 12:52:24 +0900

    feat: restrict auto-approved participants to maxParticipants

--- src/main/java/com/flab/mealmate/domain/meetup/entity/ParticipationStatus.java
+++ src/main/java/com/flab/mealmate/domain/meetup/entity/ParticipationStatus.java
@@ -1,25 +1,25 @@
 package com.flab.mealmate.domain.meetup.entity;
 
+import lombok.Getter;
 
+@Getter
 public enum ParticipationStatus {
 
-	PENDING("대기 중"),
-	APPROVED("승인"),
-	REJECTED("거절")
+	PENDING("대기 중", true),
+	APPROVED("승인", true),
+	REJECTED("거절", false)
 	;
 
-	private String description;
+	private final String description;
+	private final boolean active;
 
-	ParticipationStatus(String description) {
+	ParticipationStatus(String description, boolean active) {
 		this.description = description;
+		this.active = active;
 	}
 
-	private String description() {
-		return  this.description;
-	}
-
-	public boolean isActive() {
-		return this == PENDING || this == APPROVED;
+	public boolean isApproved() {
+		return this == ParticipationStatus.APPROVED;
 	}
 
 }
