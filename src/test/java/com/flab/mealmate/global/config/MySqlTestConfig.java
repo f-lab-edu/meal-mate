2062653f6cc8e149a438d58a5d81117e594a6528
sia <aaay8228@gmail.com>
2025-04-20 20:21:51 +0900

    chore: add missing MySqlTestConfig for MySQL ㅆTestcontainer

---
+++ src/test/java/com/flab/mealmate/global/config/MySqlTestConfig.java
@@ -0,0 +1,19 @@
+package com.flab.mealmate.global.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.flab.mealmate.aop.mysqllock.MySqlLockExecutor;
+
+@Configuration
+@Profile("mysql-test")
+public class MySqlTestConfig {
+
+	@Bean
+	public MySqlLockExecutor mySqlLockExecutor(DataSource dataSource) {
+		return new MySqlLockExecutor(dataSource);
+	}
+}
